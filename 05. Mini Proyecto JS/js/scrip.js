0801849420d8ebd2a57995b0a5001259d5bdd556
Maria Luna <180685@mail.unacar.mx>
2024-06-10 01:36:11 -0600

    recuperar mi index.html

--- 05. Mini Proyecto JS/js/scrip.js
+++ 05. Mini Proyecto JS/js/scrip.js
@@ -2,7 +2,12 @@
 const container =document.querySelector(".container"),
 musicImg = container.querySelector(".img-area img"),
 musicName = container.querySelector(".song-details .name"),
-musicArtist = container.querySelector(".song-details .artist");
+musicArtist = container.querySelector(".song-details .artist"),
+mainAudio = container.querySelector(".main-audio"),
+playpausedBTN = container.querySelector(".play-pause");
+
+
+
 
 let musicIndex = 1;
 
@@ -20,8 +25,24 @@ function loadMusic(indexNumb){
     musicImg.src = `images/${allMusic[indexNumb-1].img}.jpeg` ; 
     musicImg.src = `images/${allMusic[indexNumb-1].img}.jpeg` ; 
     musicImg.src = `images/${allMusic[indexNumb-1].img}.jpeg` ; 
-
-   
-   
-   
+    
 }
+
+function playMusic(){
+    container.classList.add("paused");
+    playpausedBTN.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+function pauseMusic(){
+    container.classList.add("paused");
+    playpausedBTN.querySelector("i").innerText = "pause";
+    mainAudio.paused();
+}
+
+playpausedBTN.addEventListener("click", ()=>{
+    const isMusicPaused = container.classList.contains("paused");
+
+    isMusicPaused ? pauseMusic() : playMusic();
+
+});
+
